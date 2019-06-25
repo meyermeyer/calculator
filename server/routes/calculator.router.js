c55de5bbaa147213986d05463045a045ff294a28
Meyer <ehlmeyer@gmail.com>
2019-06-25 13:50:13 -0500

    GET route works in postman

--- server/routes/calculator.router.js
+++ server/routes/calculator.router.js
@@ -1,18 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
+let calculationHistory = [{a:1,b:2,operator:'-',result:-1}]
+let result
 
+router.get('/', (req, res) => {
+    res.send(calculationHistory)
 });
 
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
-
+    calculateValue(req.body)
+    
 });
+
+const calculateValue = (calculationData) => {
+    let a = calculationData.a
+    let b = calculationData.b
+
+    switch (calculationData.operator) {
+        case '+':
+            result = a + b
+        case '-':
+            result = a - b
+        case '*':
+            result = a * b
+        case '/':
+            result = a / b
+    }
+    calculationHistory.push({
+        a: a,
+        b: b,
+        operator: operator,
+        result: result
+    });        
+}
 
 module.exports = router;
