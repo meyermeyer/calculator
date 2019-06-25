162299d8a20b3bd33f9cefdc01780bc4debee160
Meyer <ehlmeyer@gmail.com>
2019-06-25 12:48:12 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
